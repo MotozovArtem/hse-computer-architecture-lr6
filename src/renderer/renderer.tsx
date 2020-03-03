99850250a461057980f90625f06446837d5b7ddf
Artyom Motozov <motozov.a.v@gmail.com>
2020-03-03 23:38:46 +0300

    [-] Add bootstrap and jquery

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -6,6 +6,11 @@ import * as ReactDOM from 'react-dom';
 
 // Import the styles here to process them with webpack
 import '@public/style.css';
+import '@public/js/popper.js';
+import '@public/js/jquery.js';
+
+import '@public/css/bootstrap-grid.min.css';
+import '@public/css/bootstrap.min.css';
 import App from './App';
 
 ReactDOM.render(
